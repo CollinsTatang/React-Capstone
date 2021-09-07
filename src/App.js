f91c4afc3d672c29704a4e5f8f615d52e9c06410
Collins Tatang <makungongcollins@gmail.com>
2021-09-07 09:23:36 +0100

    Add routers

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import logo from './logo.svg';
+import
+{
+  BrowserRouter as Router, Route, Switch,
+} from 'react-router-dom';
+import Home from './components/home/Home';
+import Details from './components/details/Details';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     <h1>React Capstone</h1>
-    </div>
-  );
-}
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home,
+  },
+  {
+    path: '/details/:country',
+    name: 'Details',
+    component: Details,
+  },
+];
+
+const App = () => (
+  <Router>
+    <Switch>
+      {routes.map(({ path, component }) => (
+        <Route key={path} exact path={path} component={component} />
+      ))}
+    </Switch>
+  </Router>
+);
 
 export default App;
